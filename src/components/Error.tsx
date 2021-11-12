0ee0c6585e16c07626c36fb33a36902ce115b8c9
tigerlotuz <sara.1980@live.se>
2021-11-12 09:16:54 +0100

    Did a bouncing ball on 404 page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,9 +1,19 @@
+import { Grid, Typography } from "@mui/material";
+
 const Error = () => {
   return (
-    <section>
-      <h1>404</h1>
-      <h3>Page not found</h3>
-    </section>
+    <Grid container justifyContent="center" gap={2}>
+      <Grid item xs={12} mt={22} className="bouncing-404">
+        <Typography variant="h2" m={2}>
+          404
+        </Typography>
+      </Grid>
+      <Grid item xs={12} m={2}>
+        <Typography variant="h2" m={2}>
+          Page not found
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
