fa301613d95d2734eacf99c2800853ae0c1c8f18
tigerlotuz <sara.1980@live.se>
2021-11-12 09:22:55 +0100

    created a extra type

--- src/types/Hamster.ts
+++ src/types/Hamster.ts
@@ -1,6 +1,18 @@
 
-
 export interface Hamster {
+    id: string
+    name: string
+    age: number
+    imgName: string
+    newImg?: string
+    favFood: string
+    loves: string
+    games: number
+    wins: number
+    defeats: number
+}
+
+export interface HamsterCreate {
     id?: string
     name: string
     age: number
@@ -13,7 +25,9 @@ export interface Hamster {
     defeats: number
 }
 
+
 export interface HamsterUpdate {
     id: string
     hamster: Hamster
 }
+
