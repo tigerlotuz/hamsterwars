a364526680b7c2274167ce2d1f14d51bcc78325c
tigerlotuz <sara.1980@live.se>
2021-11-13 01:31:25 +0100

    changed language and strukture

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -14,7 +14,7 @@ const Start = () => {
     );
 
   return (
-    <Grid container justifyContent="center">
+    <Grid container justifyContent="center" spacing={2}>
       <Grid item xs={12} m={2}>
         <Typography variant="h2" mt={2} mb={2}>
           Hamster Wars
@@ -23,21 +23,17 @@ const Start = () => {
 
       {hamster && (
         <Grid item xs={12} sm={8} md={6} lg={5} xl={3} key={hamster[0].id}>
-          <Grid item xs={12}>
-            <Typography variant="h4" m={2}>
-              {hamster[0].name} is the leader
-            </Typography>
-          </Grid>
           <div className="cutest">
             <Card>
               <CardMedia
                 component="img"
                 height={400}
-                src={
-                  hamster[0].newImg
-                    ? hamster[0].newImg
-                    : `https://tigerlotuz-hamsterwars.herokuapp.com/img/${hamster[0].imgName}`
-                }
+                src={hamster[0].imgName}
+                // src={
+                //   hamster[0].newImg
+                //     ? hamster[0].newImg
+                //     : `/img/${hamster[0].imgName}`
+                // }
                 alt={hamster[0].name}
                 loading="lazy"
                 sx={{
@@ -47,18 +43,45 @@ const Start = () => {
               />
             </Card>
           </div>
-          <Grid item xs={12} mt={2} mb={2}>
-            <Typography variant="h6">
-              The war of the cutest hamster has just begun..
+          <Grid
+            item
+            xs={12}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h5" m={2}>
+              "Cutest Hamster" just nu:
             </Typography>
-            <Typography variant="h6">
-              Go to fightpage to vote for your favorite hamster
+            <Typography variant="h4" m={2}>
+              {hamster[0].name}
             </Typography>
           </Grid>
-          <Grid item xs={12} m={4}>
-            <Button onClick={() => history.push("/fight")} variant="contained">
-              Bring me to war
-            </Button>
+          <Grid item xs={12} mt={2} mb={2}>
+            <Typography variant="h6">
+              Tävlingen om vem som blir "Cutest Hamster" har startat..
+            </Typography>
+            <Typography variant="h6">
+              Gå till tävlingssidan för att rösta på din favorithamster!
+            </Typography>
+          </Grid>
+          <Grid container justifyContent="center">
+            <Grid
+              item
+              xs={12}
+              m={2}
+              sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+            >
+              <Button
+                onClick={() => history.push("/compete")}
+                variant="contained"
+                sx={{ margin: "0.5em 0.5em" }}
+              >
+                Tävla
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       )}
