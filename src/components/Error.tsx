f66a9d1362a45a79751553826eaf6a7e3ab33f9f
tigerlotuz <sara.1980@live.se>
2021-11-13 01:33:28 +0100

    added a startpage button on error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,6 +1,9 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, Button } from "@mui/material";
+import { useHistory } from "react-router-dom";
 
 const Error = () => {
+  let history = useHistory();
+
   return (
     <Grid container justifyContent="center" gap={2}>
       <Grid item xs={12} mt={22} className="bouncing-404">
@@ -12,6 +15,17 @@ const Error = () => {
         <Typography variant="h2" m={2}>
           Page not found
         </Typography>
+      </Grid>
+      <Grid container justifyContent="center" sx={{ margin: "1.5em 0" }}>
+        <Grid
+          item
+          xs={12}
+          sx={{ position: "fixed", bottom: 16, left: 0, right: 0 }}
+        >
+          <Button onClick={() => history.push("/")} variant="contained">
+            Startpage
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
