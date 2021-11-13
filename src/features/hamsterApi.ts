65f6c5f69c8b94a4ec39d319861d294451f4c0ad
tigerlotuz <sara.1980@live.se>
2021-11-13 01:30:10 +0100

    changed base url

--- src/features/hamsterApi.ts
+++ src/features/hamsterApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { Hamster, HamsterUpdate, HamsterCreate } from '../types/Hamster'
 
-const baseUrl = 'https://tigerlotuz-hamsterwars.herokuapp.com'
+const baseUrl = ""
 
 export const hamsterApi = createApi({
     reducerPath: 'hamsterApi',
